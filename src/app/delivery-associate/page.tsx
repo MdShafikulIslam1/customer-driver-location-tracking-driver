1a0f45197b46f374ca2ea1e3612cd201827275a0
shofikul <179398ssshofik@gmail.com>
2025-02-23 16:34:20 +0600

    fix: socket deployed url

--- src/app/delivery-associate/page.tsx
+++ src/app/delivery-associate/page.tsx
@@ -9,7 +9,7 @@ import { useEffect, useState } from "react";
 
 import io from "socket.io-client";
 
-const socket = io("http://localhost:5000", {
+const socket = io("https://bkend.solar-ict.com", {
   transports: ["websocket", "polling"],
 });
 
